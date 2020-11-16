e0f0b31fd333fe43cbfb83d6012ad1eaf0ecfe6e
Thomas <tpnats@gmail.com>
2020-11-16 11:47:19 -0500

    3rd commit, set up basic chart between poverty and healthcare

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -26,8 +26,20 @@ var chartGroup = svg.append("g")
 
 
 // Initial Params
-var chosenXAxis = "age";
-var chosenYAxis = "smokers";
+var chosenXAxis = "poverty";
+var chosenYAxis = "healthcare";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(overallData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(overallData, d => d[chosenXAxis]) * 0.8,
+      d3.max(overallData, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+}
 
 // Import Data
 d3.csv("./assets/data/data.csv").then(function(overallData){
@@ -43,7 +55,56 @@ d3.csv("./assets/data/data.csv").then(function(overallData){
     data.income     = +data.income;
   });
 
+   // xLinearScale function above csv import
+  var xLinearScale = xScale(overallData, chosenXAxis);
 
+   // Create y scale function
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(overallData, d => d.healthcare)])
+    .range([height, 0]);
 
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // append x axis
+  var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // append initial circles
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(overallData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", 10)
+    .attr("fill", "cyan");
+
+  // Create group for two x-axis labels
+  var labelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+  var povertyLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 10)
+    .attr("value", "poverty") // value to grab for event listener
+    .classed("active", true)
+    .text("Poverty (%)");
+
+   // append y axis
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .classed("axis-text", true)
+    .text("Health Care");
 
 });
